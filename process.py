a5772d9047047ae25b2ec15023f3c3745602df4f
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-02-18 10:20:08 -0600

    process.py complete

--- process.py
+++ process.py
@@ -1,12 +1,15 @@
+#opens txt file called um-server-01.txt; renamed to log_file
 log_file = open("um-server-01.txt")
 
-
+#function that loops through log_file
 def sales_reports(log_file):
     for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
+        line = line.rstrip() #removes whitespace from each line
+        day = line[0:3] #slices each line from index 0 to 3 which captures the abbreviation for the day; for example: Tue or Sat
+
+        #if day is Tuesday, then print the line
         if day == "Tue":
             print(line)
 
-
+#calls the function
 sales_reports(log_file)
